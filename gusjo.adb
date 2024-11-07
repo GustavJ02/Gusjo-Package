79dd3089bef5c1f778cad6bd51d3dfca8b0c82ba
= <gusjo347@student.liu.se>
2024-11-07 00:33:43 +0000

    Added operators for adding, subtracting and multiplying Integers and Floats

--- gusjo.adb
+++ gusjo.adb
@@ -1,3 +1,136 @@
 package body Gusjo is
+   -- * Operators
+   function "*"(Left : in Float;
+                Right: in Integer) return Integer is	
+		begin  
+		return Integer(Left) * Right;	
+		end "*";
    
+   function "*"(Left : in Integer;
+                Right: in Float) return Integer is	
+		begin  
+		return Right * Left;	
+		end "*";
+
+   function "*"(Left : in Float;
+                Right: in Integer) return Float is	
+		begin  
+		return Left * Float(Right);	
+		end "*";
+   
+   function "*"(Left : in Integer;
+                Right: in Float) return Float is	
+		begin  
+		return Right * Left;	
+		end "*";     
+		
+   -- + Operators
+   function "+"(Left : in Integer;	
+	        Right: in Float) return Integer is	
+		begin  
+		return Left + Integer(Right);	
+		end "+";
+		       
+   function "+"(Left : in Float;	
+	        Right: in Integer) return Integer is	
+		begin  
+		return Right + Left;	
+		end "+";
+		       
+   function "+"(Left : in Integer;	
+	        Right: in Float) return Float is	
+		begin  
+		return Float(Left) + Right;	
+		end "+";
+		       
+   function "+"(Left : in Float;	
+	        Right: in Integer) return Float is	
+		begin  
+		return Right + Left;	
+		end "+";
+   -- - Operators       
+   function "-"(Left : in Integer;	
+	        Right: in Float) return Integer is	
+		begin  
+		return Left - Integer(Right);	
+		end "-";
+		       
+   function "-"(Left : in Float;	
+	        Right: in Integer) return Integer is	
+		begin  
+		return Integer(Left) - Right;	
+		end "-";
+		       
+   function "-"(Left : in Integer;	
+	        Right: in Float) return Float is
+		begin  
+		return Float(Left) - Right;	
+		end "-";
+		       
+   function "-"(Left : in Float;	
+	        Right: in Integer) return Float is	
+		begin  
+		return Left - Float(Right);	
+		end "-";    
+		
+      -- Comparators      
+   function ">"(Left : in Float;	
+                Right: in Integer) return Boolean is	
+		begin  
+		return Left > Float(Right);	
+		end ">";	
+		       
+   function ">"(Left : in Integer;	
+                Right: in Float) return Boolean is	
+		begin  
+		return Float(Left) > Right;	
+		end ">";	   
+		    
+   function ">="(Left : in Float;	
+                Right: in Integer) return Boolean is	
+		begin  
+		return Left >= Float(Right);	
+		end ">=";	
+		       
+   function ">="(Left : in Integer;	
+                Right: in Float) return Boolean is	
+		begin  
+		return Float(Left) >= Right;	
+		end ">=";	   
+		    
+   function "<"(Left : in Float;	
+                Right: in Integer) return Boolean is	
+		begin  
+		return Left < Float(Right);	
+		end "<";	
+		       
+   function "<"(Left : in Integer;	
+                Right: in Float) return Boolean is	
+		begin  
+		return Float(Left) < Right;	
+		end "<";	   
+		    
+   function "<="(Left : in Float;	
+                Right: in Integer) return Boolean is	
+		begin  
+		return Left <= Float(Right);	
+		end "<=";	
+		       
+   function "<="(Left : in Integer;	
+                Right: in Float) return Boolean is	
+		begin  
+		return Float(Left) <= Right;	
+		end "<=";	   
+		    
+   function "="(Left : in Float;	
+                Right: in Integer) return Boolean is	
+		begin  
+		return Left = Float(Right);	
+		end "=";
+		       
+   function "="(Left : in Integer;	
+                Right: in Float) return Boolean is	
+		begin  
+		return Float(Left) = Right;	
+		end "=";
 end Gusjo;
--- gusjo.ads
+++ gusjo.ads
@@ -3,15 +3,71 @@ package Gusjo is
    Null_Pointer_Exception : exception;
    Internal_Error         : exception;
    
-   funciton "*"(Left : in Float;
+   -- * Operators
+   function "*"(Left : in Float;
                 Right: in Integer) return Integer;
    
-   funciton "*"(Left : in Integer;
+   function "*"(Left : in Integer;
                 Right: in Float) return Integer;
 
-   funciton "*"(Left : in Float;
+   function "*"(Left : in Float;
                 Right: in Integer) return Float;
    
-   funciton "*"(Left : in Integer;
-                Right: in Float) return Float;
+   function "*"(Left : in Integer;
+                Right: in Float) return Float;	
+   -- + Operators
+   function "+"(Left : in Integer;	
+	        Right: in Float) return Integer;	
+		       
+   function "+"(Left : in Float;	
+	        Right: in Integer) return Integer;
+		       
+   function "+"(Left : in Integer;	
+	        Right: in Float) return Float;	
+		       
+   function "+"(Left : in Float;	
+	        Right: in Integer) return Float;	
+   -- - Operators       
+   function "-"(Left : in Integer;	
+	        Right: in Float) return Integer;	
+		       
+   function "-"(Left : in Float;	
+	        Right: in Integer) return Integer;	
+		       
+   function "-"(Left : in Integer;	
+	        Right: in Float) return Float;	
+		       
+   function "-"(Left : in Float;	
+	        Right: in Integer) return Float;	
+		       
+   -- Comparators      
+   function ">"(Left : in Float;	
+                Right: in Integer) return Boolean;	
+		       
+   function ">"(Left : in Integer;	
+                Right: in Float) return Boolean;	
+		       
+   function ">="(Left : in Float;	
+                 Right: in Integer) return Boolean;	
+		       
+   function ">="(Left : in Integer;	
+                 Right: in Float) return Boolean;	
+		       
+   function "<"(Left : in Float;	
+                Right: in Integer) return Boolean;	
+		       
+   function "<"(Left : in Integer;	
+                Right: in Float) return Boolean;
+		       
+   function "<="(Left : in Float;	
+                 Right: in Integer) return Boolean;
+		       
+   function "<="(Left : in Integer;	
+                 Right: in Float) return Boolean;
+		       
+   function "="(Left : in Float;	
+                Right: in Integer) return Boolean;
+		       
+   function "="(Left : in Integer;	
+                Right: in Float) return Boolean;
 end Gusjo;
