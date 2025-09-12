db987d711c4decc72603b154c13d5713e3bd7314
Gustav Johansson <gustav.1.johansson@outlook.com>
2025-09-12 21:17:36 +0200

    Using new functions and procedures in linalg module

--- ai/gusjo-ai-neuron.adb
+++ ai/gusjo-ai-neuron.adb
@@ -6,32 +6,20 @@ with Gusjo.Math.Linalg;                 use Gusjo.Math.Linalg;
 
 package body Gusjo.Ai.Neuron is
 
-   procedure Alloc_Weights(Nn : in out Neuron;
-                           N  : in     Positive) is
-      Tmp : Matrix := Zeros(N, 1);
-   begin
-      if Nn = null then
-         raise Null_Pointer_Exception;
-      end if;
-
-      if Nn.W /= null then
-         Delete(Nn.W);
-      end if;
-
-      Nn.W := To_Column_Vector(Tmp);
-      Delete(Tmp);
-   end Alloc_Weights;
+   procedure Free is new Ada.Unchecked_Deallocation(Neuron_Type, Neuron);
 
    procedure Initialize(N        : in out Neuron;
                         Inputs   : in     Positive;
                         Use_Bias : in     Boolean := true) is
+      Tmp : Matrix := Zeros(N, 1);
    begin
-      if N = null then
-         Delete(N);
+      if N /= null then
+         Free(N);
       end if;
 
       N := New Neuron_Type;
-      Alloc_Weights(N, Inputs);
+      N.W := To_Column_Vector(Tmp);
+      Delete(Tmp);
       N.B := 0.0;
       N.Use_Bias := Use_Bias;
    end Initialize;
@@ -42,12 +30,9 @@ package body Gusjo.Ai.Neuron is
       Gen : Ada.Numerics.Float_Random.Generator;
       Rng : constant Float := High - Low;
    begin
-      Ada.Numerics.Float_Random.Reset(Gen);
-      for I in N.W'Range(1) loop
-         N.W(I, 1) := Low + Rng * Ada.Numberics.Float_Random.Random(Gen);
-      end loop;
+      Fill_Random_Uniform (N.W, Low, High);
       if N.Use_Bias then
-         N.B := Low + Rng * Ada.Numberics.Float_Random.Random(Gen);
+         N.B := Low + (Rng * Ada.Numerics.Float_Random.Random(Gen));
       else
          N.B := 0.0;
       end if;
@@ -84,14 +69,11 @@ package body Gusjo.Ai.Neuron is
                     Learning_Rate   : in     Float := 0.01) is
       Y_Pred   : constant Float := Forward(N, X);
       Err      : constant Float := Y_Pred - Y_True;
-      LR       : constant Float := Learning_Rate;
    begin
-      for I in N.W'Range(1) loop
-         N.W(I, 1) := N.W(I, 1) - LR * Err * X(I, 1);
-      end loop;
+      Axpy(Y => N.W, Alpha => -Learning_Rate * Err, X => X);
 
       if N.Use_Bias then
-         N.B := N.B - LR * Err;
+         N.B := N.B - Learning_Rate * Err;
       end if;
    end Update;
 
