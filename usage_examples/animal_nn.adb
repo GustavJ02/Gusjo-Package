acad3e84ed72c84d8f7fa261b0b7ec2cde2a3366
Gustav Johansson <gustav.1.johansson@outlook.com>
2026-05-13 21:43:06 +0200

    Stated on program to run anmimal image classifier

---
+++ usage_examples/animal_nn.adb
@@ -0,0 +1,19 @@
+with Ada.Text_IO;          use Ada.Text_IO;
+with Ada.Float_Text_IO;    use Ada.Float_Text_IO;
+
+with Gusjo.Ai;             use Gusjo.Ai;
+with Gusjo.Ai.Nn;          use Gusjo.Ai.Nn;
+with Gusjo.Math;           use Gusjo.Math;
+with Gusjo.Data.Frame;     use Gusjo.Data.Frame;
+with Gusjo.Math.Linalg;    use Gusjo.Math.Linalg;
+
+procedure Animal_Nn is
+
+   DF : DataFrame_Type;
+
+begin
+   Put_Line("Loading animals dataset...");
+   Load_CSV("python_comparison/animals.csv", DF);
+
+   Delete(DF);
+end Animal_Nn;
